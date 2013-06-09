17dbb7f97e6562f8c2d712dc538eab2c9429a9b3
Owen <your.name@example.com>
2013-06-09 14:01:43 -0700

    Finish Ajax GET

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AjaxTakeone::Application.config.secret_token = 'dd8f8c426f4c17693a564b724e0deb68947de1edead0cf42f38452663ab6a3f450113260805656cb35efb167176a33ccdccfe3cd1b97ce53db9019006843b3d6'
